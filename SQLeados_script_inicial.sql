3a3ee99dc2cbfd46982c41876792d03ed459b15a
damianKreuter <damiankreuter@outlook.com>
2018-10-21 22:57:22 -0300

    Correción de nombres

--- SQLeados_script_inicial.sql
+++ SQLeados_script_inicial.sql
@@ -4,9 +4,9 @@ GO
 ----------------------------------------------------------------------------------------------
 								/** CREACION DE SCHEMA **/
 ----------------------------------------------------------------------------------------------
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'SQLeados')
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'SQLEADOS')
 BEGIN
-    EXEC ('CREATE SCHEMA SQLeados AUTHORIZATION gd')
+    EXEC ('CREATE SCHEMA SQLeados AUTHORIZATION gdEspectaculos2018')
 END
 GO
 
@@ -15,83 +15,83 @@ GO
 								/** VAlidacion tablas **/
 ----------------------------------------------------------------------------------------------
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.FuncionalidadXRol'))
-    DROP TABLE SQLeados.FuncionalidadXRol
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.FuncionalidadXRol'))
+    DROP TABLE SQLEADOS.FuncionalidadXRol
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Compra'))
-    DROP TABLE SQLeados.Compra
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Compra'))
+    DROP TABLE SQLEADOS.Compra
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.ubicacionXpublicacion'))
-    DROP TABLE SQLeados.ubicacionXpublicacion
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.ubicacionXpublicacion'))
+    DROP TABLE SQLEADOS.ubicacionXpublicacion
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Publicacion'))
-    DROP TABLE SQLeados.Publicacion
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Publicacion'))
+    DROP TABLE SQLEADOS.Publicacion
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Empresa'))
-    DROP TABLE SQLeados.Empresa
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Empresa'))
+    DROP TABLE SQLEADOS.Empresa
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Cliente'))
-    DROP TABLE SQLeados.Cliente
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Cliente'))
+    DROP TABLE SQLEADOS.Cliente
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Usuario'))
-    DROP TABLE SQLeados.Usuario
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Usuario'))
+    DROP TABLE SQLEADOS.Usuario
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Rol'))
-    DROP TABLE SQLeados.Rol
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Rol'))
+    DROP TABLE SQLEADOS.Rol
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Funcionalidad'))
-    DROP TABLE SQLeados.Funcionalidad
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Funcionalidad'))
+    DROP TABLE SQLEADOS.Funcionalidad
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Ubicacion'))
-    DROP TABLE SQLeados.Ubicacion
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Ubicacion'))
+    DROP TABLE SQLEADOS.Ubicacion
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.GradoPrioridad'))
-    DROP TABLE SQLeados.GradoPrioridad
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.GradoPrioridad'))
+    DROP TABLE SQLEADOS.GradoPrioridad
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Rubro'))
-    DROP TABLE SQLeados.Rubro
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Rubro'))
+    DROP TABLE SQLEADOS.Rubro
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Domicilio'))
-    DROP TABLE SQLeados.Domicilio
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Domicilio'))
+    DROP TABLE SQLEADOS.Domicilio
 
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLeados.Funcionalidad'))
-    DROP TABLE SQLeados.Funcionalidad
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SQLEADOS.Funcionalidad'))
+    DROP TABLE SQLEADOS.Funcionalidad
 
 
 ----------------------------------------------------------------------------------------------
 								/** CREACION de tablas **/
 ----------------------------------------------------------------------------------------------
 
-create table [SQLeados].Funcionalidad(
+create table [SQLEADOS].Funcionalidad(
 funcionalidad_Id int primary key identity,
 funcionalidad_descripcion nvarchar(255) not null,)
 
 
 
 
-create table [SQLeados].Rol(
+create table [SQLEADOS].Rol(
 rol_Id int primary key identity,
 rol_nombre nvarchar(255) not null,
 rol_estado bit default 1
 )
 
-create table [SQLeados].FuncionalidadXRol(
+create table [SQLEADOS].FuncionalidadXRol(
 funcionalidadXRol_Id int primary key identity,
-funcionalidadXRol_rol int not null references [SQLeados].Rol,
-funcionalidadXRol_funcionalidad int not null references [SQLeados].Funcionalidad ,
+funcionalidadXRol_rol int not null references [SQLEADOS].Rol,
+funcionalidadXRol_funcionalidad int not null references [SQLEADOS].Funcionalidad ,
 )
 
-create table [SQLeados].Usuario(
+create table [SQLEADOS].Usuario(
 usuario_Id int primary key identity,
 usuario_username varchar(255) unique not null,
 usuario_password varchar(255) not null,
-usuario_rol int not null references [SQLeados].Rol,
+usuario_rol int not null references [SQLEADOS].Rol,
 usuario_tipo varchar(20) not null,
 usuario_estado int default 1,
 usuario_intentos int default 0,
 )
 
-create table [SQLeados].Domicilio(
+create table [SQLEADOS].Domicilio(
 domicilio_id int primary key identity,
 domicilio_calle varchar(255) not null,
 domicilio_numero int not null,
@@ -101,10 +101,10 @@ domicilio_localidad varchar(255),
 domicilio_codigo_postal int not null,
 )
 
-create table [SQLeados].Cliente(
+create table [SQLEADOS].Cliente(
 cliente_nombre varchar(255) not null,
 cliente_apellido varchar(255) not null,
-cliente_usuario int not null references [SQLeados].Usuario,
+cliente_usuario int not null references [SQLEADOS].Usuario,
 cliente_tipo_documento varchar(5) not null,
 cliente_numero_documento numeric(18,0) not null,
 cliente_fecha_nacimiento datetime not null,
@@ -115,31 +115,31 @@ cliente_email varchar(255) not null,
 cliente_telefono varchar(255),
 cliente_estado int default 1,
 cliente_cuit varchar(20),
-cliente_domicilio int references [SQLeados].Domicilio,
+cliente_domicilio int references [SQLEADOS].Domicilio,
 PRIMARY KEY (cliente_tipo_documento,cliente_numero_documento)
 )
 
-create table [SQLeados].Empresa(
+create table [SQLEADOS].Empresa(
 empresa_cuit nvarchar(255) primary key,
 empresa_razon_social varchar(255) not null,
-empresa_usuario int not null references [SQLeados].Usuario,
-empresa_domicilio int references [SQLeados].Domicilio,
+empresa_usuario int not null references [SQLEADOS].Usuario,
+empresa_domicilio int references [SQLEADOS].Domicilio,
 empresa_ciudad varchar(255) not null,
 empresa_estado int default 1,
 )
 
-create table [SQLeados].Rubro(
+create table [SQLEADOS].Rubro(
 rubro_id int primary key identity,
 rubro_descripcion varchar(255) not null,
 )
 
-create table [SQLeados].GradoPrioridad(
+create table [SQLEADOS].GradoPrioridad(
 grado_id int primary key identity,
 grado_nombre varchar(255) not null,
 grado_comision numeric(10,2) not null,
 )
 
-create table [SQLeados].Ubicacion(
+create table [SQLEADOS].Ubicacion(
 ubicacion_id int primary key identity,
 ubicacion_fila nvarchar(3) not null,
 ubicacion_asiento numeric(18,0) not null,
@@ -149,45 +149,35 @@ ubicacion_Tipo_codigo numeric(18,0),
 ubicacion_Tipo_Descripcion nvarchar(255),
 )
 
-create table [SQLeados].Publicacion(
+create table [SQLEADOS].Publicacion(
 publicacion_codigo int primary key identity,
-publicacion_usuario_responsable int references [SQLeados].Usuario,
-publicacion_rubro int references [SQLeados].Rubro not null,
-publicacion_grado int references [SQLeados].GradoPrioridad not null,
+publicacion_usuario_responsable int references [SQLEADOS].Usuario,
+publicacion_rubro int references [SQLEADOS].Rubro not null,
+publicacion_grado int references [SQLEADOS].GradoPrioridad not null,
 publicacion_descripcion varchar(255),
 publicacion_precio numeric(20,2) not null,
 publicacion_stock int not null,
 publicacion_estado varchar(20) not null,
 publicacion_puntaje_venta int not null,
-publicacion_ubicaciones int references [SQLeados].Ubicacion not null,
+publicacion_ubicaciones int references [SQLEADOS].Ubicacion not null,
 publicacion_fecha datetime not null
 )
 
-create table [SQLeados].ubicacionXpublicacion(
+create table [SQLEADOS].ubicacionXpublicacion(
 ubiXpubli_ID int primary key identity,
-ubiXpubli_Ubicacion int references [SQLeados].Ubicacion,
-ubiXpubli_Publicacion int references [SQLeados].Publicacion,
+ubiXpubli_Ubicacion int references [SQLEADOS].Ubicacion,
+ubiXpubli_Publicacion int references [SQLEADOS].Publicacion,
 )
 
-create table [SQLeados].Compra(
+create table [SQLEADOS].Compra(
 compra_id int primary key identity,
 compra_cliente_tipo_documento varchar(5),
 compra_cliente_numero_documento numeric(18,0),
-compra_publicacion_codigo int references [SQLeados].Publicacion,
+compra_publicacion_codigo int references [SQLEADOS].Publicacion,
 compra_fecha datetime not null,
 compra_cantidad numeric(18,0) not null,
-FOREIGN KEY (compra_cliente_tipo_documento, compra_cliente_numero_documento) REFERENCES [SQLeados].Cliente(cliente_tipo_documento,cliente_numero_documento),
+FOREIGN KEY (compra_cliente_tipo_documento, compra_cliente_numero_documento) REFERENCES [SQLEADOS].Cliente(cliente_tipo_documento,cliente_numero_documento),
 )
-
-
-
-
-
-
-
-
-
-
 
 
 
