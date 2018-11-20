675d73710e4ef92dd8bfe316ac78cb3420087a57
damianKreuter <damiankreuter@outlook.com>
2018-11-20 20:38:40 -0300

    Revert "revert"
    This reverts commit ffaf7384e62817b32aeaccd770c17d1c0335ff33.

--- PalcoNet/Login y seguridad/SeleccionarROL.cs
+++ PalcoNet/Login y seguridad/SeleccionarROL.cs
@@ -15,6 +15,30 @@ namespace PalcoNet.Login_y_seguridad
 
     {
         String user;
+        public class ComboboxRol
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+
+        private void Test()
+        {
+            ComboboxRol item = new ComboboxRol();
+            item.Text = "Rol a seleccionar";
+            item.Value = 12;
+
+            comboBoxRol.Items.Add(item);
+
+            comboBoxRol.SelectedIndex = 0;
+
+            MessageBox.Show((comboBoxRol.SelectedItem as ComboboxRol).Value.ToString());
+        }
+
         public SeleccionarROL(String usuario)
         {
             user = usuario;
@@ -23,7 +47,12 @@ namespace PalcoNet.Login_y_seguridad
 
         private void SeleccionarROL_Load(object sender, EventArgs e)
         {
+            DataSet myDataSet = LoginSQL.ObtenerRoles(user);
+ 
+            comboBoxRol.DataSource = myDataSet.Tables[0].DefaultView;
+            comboBoxRol.DisplayMember = "Rol a seleccionar";
 
+            comboBoxRol.BindingContext = this.BindingContext;
         }
 
         private void volver_boton_Click_1(object sender, EventArgs e)
@@ -34,6 +63,16 @@ namespace PalcoNet.Login_y_seguridad
         private void comboBoxRol_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        //Boton Ingresar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBoxRol.SelectedItem.ToString() == "") {
+                MessageBox.Show("Seleccione un rol para continuar");
+                return;
+            }
+            String rolSeleccionado = comboBoxRol.SelectedItem.ToString();
         }
     }
 }
