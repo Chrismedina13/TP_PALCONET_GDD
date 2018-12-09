8c9277bc75d3c4d0963b4bde058e812d75ec5464
damianKreuter <damiankreuter@outlook.com>
2018-12-08 23:01:28 -0300

    Reparo los archivos corruptos debido al Merge

---
+++ PalcoNet/Generar Publicacion/tablaPublicacion.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PalcoNet.Generar_Publicacion
+{
+    partial class tablaPublicacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonComprar = new System.Windows.Forms.Button();
+            this.buttonUltimaHoja = new System.Windows.Forms.Button();
+            this.buttonPrimeraHoja = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelPaginas = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.botonsiguiente = new System.Windows.Forms.Button();
+            this.botonAnterior = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonComprar
+            // 
+            this.buttonComprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonComprar.Location = new System.Drawing.Point(842, 479);
+            this.buttonComprar.Name = "buttonComprar";
+            this.buttonComprar.Size = new System.Drawing.Size(127, 45);
+            this.buttonComprar.TabIndex = 56;
+            this.buttonComprar.Text = "Comprar";
+            this.buttonComprar.UseVisualStyleBackColor = true;
+            // 
+            // buttonUltimaHoja
+            // 
+            this.buttonUltimaHoja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUltimaHoja.Location = new System.Drawing.Point(640, 482);
+            this.buttonUltimaHoja.Name = "buttonUltimaHoja";
+            this.buttonUltimaHoja.Size = new System.Drawing.Size(108, 45);
+            this.buttonUltimaHoja.TabIndex = 55;
+            this.buttonUltimaHoja.Text = "Última hoja";
+            this.buttonUltimaHoja.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrimeraHoja
+            // 
+            this.buttonPrimeraHoja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPrimeraHoja.Location = new System.Drawing.Point(252, 482);
+            this.buttonPrimeraHoja.Name = "buttonPrimeraHoja";
+            this.buttonPrimeraHoja.Size = new System.Drawing.Size(127, 45);
+            this.buttonPrimeraHoja.TabIndex = 54;
+            this.buttonPrimeraHoja.Text = "Primera hoja";
+            this.buttonPrimeraHoja.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 138);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(957, 335);
+            this.dataGridView1.TabIndex = 53;
+            // 
+            // labelPaginas
+            // 
+            this.labelPaginas.AutoSize = true;
+            this.labelPaginas.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelPaginas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelPaginas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPaginas.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelPaginas.Location = new System.Drawing.Point(438, 500);
+            this.labelPaginas.MinimumSize = new System.Drawing.Size(150, 20);
+            this.labelPaginas.Name = "labelPaginas";
+            this.labelPaginas.Size = new System.Drawing.Size(150, 22);
+            this.labelPaginas.TabIndex = 52;
+            this.labelPaginas.Text = "1/1";
+            this.labelPaginas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(465, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "PÁGINA";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(12, 479);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 48);
+            this.button3.TabIndex = 50;
+            this.button3.Text = "Volver";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // botonsiguiente
+            // 
+            this.botonsiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonsiguiente.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.botonsiguiente.Location = new System.Drawing.Point(589, 482);
+            this.botonsiguiente.Name = "botonsiguiente";
+            this.botonsiguiente.Size = new System.Drawing.Size(45, 45);
+            this.botonsiguiente.TabIndex = 49;
+            this.botonsiguiente.Text = ">";
+            this.botonsiguiente.UseVisualStyleBackColor = true;
+            // 
+            // botonAnterior
+            // 
+            this.botonAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAnterior.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.botonAnterior.Location = new System.Drawing.Point(387, 482);
+            this.botonAnterior.Name = "botonAnterior";
+            this.botonAnterior.Size = new System.Drawing.Size(45, 45);
+            this.botonAnterior.TabIndex = 48;
+            this.botonAnterior.Text = "<";
+            this.botonAnterior.UseVisualStyleBackColor = true;
+            // 
+            // tablaPublicacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 533);
+            this.Controls.Add(this.buttonComprar);
+            this.Controls.Add(this.buttonUltimaHoja);
+            this.Controls.Add(this.buttonPrimeraHoja);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelPaginas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.botonsiguiente);
+            this.Controls.Add(this.botonAnterior);
+            this.Name = "tablaPublicacion";
+            this.Text = "tablaPublicacion";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button buttonComprar;
+        public System.Windows.Forms.Button buttonUltimaHoja;
+        public System.Windows.Forms.Button buttonPrimeraHoja;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.Label labelPaginas;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button button3;
+        public System.Windows.Forms.Button botonsiguiente;
+        public System.Windows.Forms.Button botonAnterior;
+    }
+}
